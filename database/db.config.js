b9a0338d586608e1f5cd9812cefa81c5a2cbb266
Bright Peter <brightbassey2001@gmail.com>
2022-11-10 11:04:46 +0100

    set up the node server and sequelize connection

--- database/db.config.js
+++ database/db.config.js
@@ -1,1 +1,18 @@
 // TODO postgres connection config file
+const { Sequelize } = require('sequelize');
+
+require('dotenv').config();
+
+// using the data from the .env file to configure our database
+const databaseName = process.env.DATABASE_NAME;
+
+const username = process.env.DATABASE_USERNAME;
+
+const password = process.env.DATABASE_PASSWORD;
+
+const host = process.env.HOST;
+
+// creating an instance of our sequelize connection
+const sequelize = new Sequelize(databaseName, username, password, { host, dialect: 'postgres' });
+
+module.exports = { sequelize };
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 // TODO setup server
+const express = require('express');
+
+const { sequelize } = require('../database/db.config');
+
+const app = express();
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+// database connection using configuration in db.config.js
+const databaseConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('successfully connected to database');
+  } catch (error) {
+    console.error('unable to connect to database');
+  }
+};
+
+databaseConnection();
+
+// To check that node server is running successfully
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
