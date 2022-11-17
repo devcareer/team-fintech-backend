c9dfa99654d896896a6275c43ee390de8a8fe05a
kelue <austincmoon5@gmail.com>
2022-11-17 12:48:51 +0100

    refactored express app to module

--- src/server.js
+++ src/server.js
@@ -6,11 +6,4 @@ const { sequelize } = require('../database/db.config');
 
 const app = express();
 
-require('dotenv').config();
-
-const port = process.env.PORT || 8080;
-
-// To check that node server is running successfully
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+module.exports = app;
