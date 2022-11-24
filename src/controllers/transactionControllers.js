003c093b6bff2103163bfb9712d60779c49b0212
Bright Peter <brightbassey2001@gmail.com>
2022-11-24 23:51:27 +0100

    #31 added function and route for transferring data using phone number

--- src/controllers/transactionControllers.js
+++ src/controllers/transactionControllers.js
@@ -37,4 +37,29 @@ const transferAirtime = async (req, res, next) => {
   }
 };
 
-module.exports = { transferAirtime };
+const transferData = async (req, res, next) => {
+  try {
+    const reference = uuidv4();
+    const { country, customer, amount, type, recurrence } = req.body;
+    const payload = {
+      country,
+      customer,
+      amount,
+      type,
+      reference,
+      recurrence,
+    };
+
+    const response = await flw.Bills.create_bill(payload);
+
+    if (response.status === 'error') {
+      res.status(ERROR).json(response);
+    } else {
+      res.status(SUCCESS).json(response);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { transferAirtime, transferData };
